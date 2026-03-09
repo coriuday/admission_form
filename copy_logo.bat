8f9b79d84b71fde29fd7ccdf3e8e68f287733d50
Uday Kumar Kori <coriuday.18@gmail.com>
2026-03-09 22:05:24 +0530

    chore: remove temp helper scripts

--- copy_logo.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if not exist "public" mkdir public
-copy /Y "items\GNU_Logo_PDF_page-0001-removebg-preview.png" "public\gnu-logo.png"
-echo Done! Logo copied to public folder.
-dir public
